d2607d6dd29307da74633165347e31fdb3617488
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-08-05 20:29:55 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -8,10 +8,12 @@ from info import ADMINS
 async def restart(bot, message):
     if message.from_user.id != ADMINS:
         await message.reply_text("sorry bro this bot is admin only use. Please make your owen bot.\nbot repo https://github.com/MrMKN/Eva-Forward-Bot ", disable_web_page_preview = True)                      
-        return 
-    msg = await message.reply_text(
-        text="<i>Trying to restarting.....</i>"
-    )
-    await asyncio.sleep(5)
-    await msg.edit("<i>Server restarted successfully ✅</i>")
-    os.execl(sys.executable, sys.executable, *sys.argv)
+        return
+    while True:
+        try:
+            msg = await message.reply_text(
+            text="<i>Trying to restarting.....</i>"
+            )
+            await asyncio.sleep(5)
+            await msg.edit("<i>Server restarted successfully ✅</i>")
+            os.execl(sys.executable, sys.executable, *sys.argv)
