1ffacfbb3df15afd1a163b9b528f7b73224e36d8
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-08-05 16:40:54 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -2,9 +2,13 @@ import os
 import sys
 import asyncio
 from pyrogram import Client, filters
+from info import ADMINS
 
-@Client.on_message(filters.private & filters.command(['restart']))
+@Client.on_message(filters.private & filters.command(['restart']))                         
 async def restart(bot, message):
+    if message.from_user.id != ADMINS:
+        await message.reply_text("sorry bro this bot is admin only use. Please make your owen bot.\nbot repo https://github.com/MrMKN/Eva-Forward-Bot ", disable_web_page_preview = True)                      
+        return 
     msg = await message.reply_text(
         text="<i>Trying to restarting.....</i>"
     )
